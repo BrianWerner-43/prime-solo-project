196be6f6a00629537e684106c2bba4ef07b8a427
Brian Werner <brianmw199@gmail.com>
2024-01-08 22:39:32 -0600

    Added a sweet alert instead and styled the page using complex interaction card with MUI

--- src/components/RecipeDetails/RecipeDetails.jsx
+++ src/components/RecipeDetails/RecipeDetails.jsx
@@ -1,7 +1,22 @@
-import { useEffect, useState } from 'react';
+import * as React from 'react';
+import { styled } from '@mui/material/styles';
+import Card from '@mui/material/Card';
+import CardHeader from '@mui/material/CardHeader';
+import CardMedia from '@mui/material/CardMedia';
+import CardContent from '@mui/material/CardContent';
+import CardActions from '@mui/material/CardActions';
+import Collapse from '@mui/material/Collapse';
+import Avatar from '@mui/material/Avatar';
+import IconButton from '@mui/material/IconButton';
+import Typography from '@mui/material/Typography';
+import { red } from '@mui/material/colors';
+import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
+import { useEffect } from 'react';
 import { useDispatch, useSelector} from 'react-redux';
 import { useParams, useHistory } from 'react-router-dom';
 import './RecipeDetails.css';
+import Swal from 'sweetalert2';
+
 
 
 // Creating a function that will display the recipe and recipe detals
@@ -10,32 +25,55 @@ function RecipeDetails() {
   const history = useHistory();
   const dispatch = useDispatch();
   const ID = useParams();
-  const [showModal, setShowModal] = useState(false);
   const details = useSelector(store => store.setDetailsReducer);
   
-
-  // Delete function
-   // also use a modal to give the user the option if they want to delete recipe or not
-  const handleDelete = () => {
-    setShowModal(true);
-  }
-  
-   const handleConfirmDelete = () => {
-    
-    dispatch({
-      type: 'DELETE_RECIPE',
-      payload: details.id
-    })
-    history.push('/');
-
+// Complex Interaction card material UI function
+  const ExpandMore = styled((props) => {
+    const { expand, ...other } = props;
+    return <IconButton {...other} />;
+  })(({ theme, expand }) => ({
+    transform: !expand ? 'rotate(0deg)' : 'rotate(180deg)',
+    marginLeft: 'auto',
+    transition: theme.transitions.create('transform', {
+      duration: theme.transitions.duration.shortest,
+    }),
+  }));
+  const [expanded, setExpanded] = React.useState(false);
+  const handleExpandClick = () => {
+    setExpanded(!expanded);
   };
 
-  const handleCancelDelete = () => {
-    setShowModal(false);
-  }
+  // Delete function that will delete the recipe
+   // As well as using a sweet alet to give the user the option to delete or not
+   const handleDelete = () => {
+    
+    Swal.fire({
+      title: `Are you sure you want to delete ${details.title} ?`,
+      text: "You won't be able to revert this!",
+      icon: "warning",
+      showCancelButton: true,
+      confirmButtonColor: "#008000",
+      cancelButtonColor: "#d33",
+      confirmButtonText: "Yes, delete it!"
+    }).then((result) => {
+      if (result.isConfirmed) {
+        Swal.fire({
+          title: "Deleted!",
+          text: "Your file has been deleted.",
+          icon: "success"
+        }).then(() => {
+          dispatch({
+            type: 'DELETE_RECIPE',
+            payload: details.id
+          });
+          history.push("/");
+          
+        });
+      }
+    });
+   };
 
   // Edit function that when clicked it will bring the user to the edit recipe page
-  
   const handleEditBtn = (id) => {
     history.push(`/edit/${id}`)
     
@@ -47,8 +85,7 @@ function RecipeDetails() {
   }
 
 // Sending a call to the store to retrieve the recipe details for the user
-  
-  useEffect(() =>{
+   useEffect(() =>{
     console.log('Expect to get the ids:', ID);
     dispatch({
       type: "SAGA_GET_DETAILS",
@@ -57,37 +94,62 @@ function RecipeDetails() {
     
   }, [])
   
-
+// The render for the Complex Interaction card from material UI
   return (
   <div className="container">
-    <div>
+    <div className="image-border">
+    <Card sx={{ maxWidth: 400 }}>
+      <CardHeader
+        avatar={
+          <Avatar sx={{ bgcolor: red[500] }} aria-label="recipe">
+            cwc
+          </Avatar>
+        }
+        title={details.title}  
+      />
+       <CardMedia
+       sx={{height: 300}}
+       image={details.image_url}  
+      />
+      <CardContent>
+        <Typography variant="body2" color="text.secondary">
+          
+        </Typography>
+      </CardContent>
+      <CardActions disableSpacing>
+        <ExpandMore
+          expand={expanded}
+          onClick={handleExpandClick}
+          aria-expanded={expanded}
+          aria-label="show more"
+        >
+          <ExpandMoreIcon />
+        </ExpandMore>
+        </CardActions>
+          <Collapse in={expanded} timeout="auto" unmountOnExit>
+        <CardContent>
+          <Typography paragraph>Method:</Typography>
+          <Typography paragraph>
+          {details.description}
+          </Typography>
+      </CardContent>
+      </Collapse>
+    </Card>
       
-      <img src={details.image_url} />
-      <h1>{details.title}</h1>
-      <h2>Method:</h2>
-      <p>{details.description}</p>
-
-      <button className="back-btn" onClick={() => history.goBack()}>👈</button>
-      <button className="delete-btn" onClick={handleDelete}>Delete</button>
-      <button className="edit-btn"  onClick={handleEditBtn}>Edit</button>
+      <button className="back-btn" onClick={() => history.goBack()}>👈 Go back</button>
       <button className="add-btn" onClick={handleAddBtn}>Add Recipe</button>
-      {/* Modal for confirming and canceling the delete */}
-      {showModal&& (
-      <div className="modal">
-       <div className="modal-content">
-       <h2>Are you sure you want to delete {details.title}</h2>
-       <button className="modalBtn" type="button" onClick={handleConfirmDelete}>Yes delete</button>
-       <button className="modalBtn" type="button" onClick={handleCancelDelete}>No thanks</button>
-
-       </div>
-      </div>
-    )}
+      <br></br>
+      <button className="edit-btn"  onClick={handleEditBtn}>Edit</button>
+      <button className="delete-btn" onClick={handleDelete}>Delete</button>
       
      </div>
     </div>
     
-  )
-  
+  );
 }
+    
+  
+  
+
 
 export default RecipeDetails;
